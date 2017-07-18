0a6872d1fd61ea15025af13b8bedf99b132d5d7b
ToddSandberg <sandbergs.todd@gmail.com>
2017-07-18 08:32:00 -0400

    updated README.md

--- paraphrase/src/paraphrase/App.java
+++ paraphrase/src/paraphrase/App.java
@@ -10,9 +10,9 @@ import java.util.List;
 public class App {
     public static void main(String [] args){
         Paraphrase pp = new Paraphrase();
-        System.out.println("take: "+Paraphrase.termToString("take", "verb"));
+        System.out.println("was: "+Paraphrase.termToString("was", "verb"));
         System.out.println("arrive: "+Paraphrase.termToString("arrive", "verb"));
         System.out.println("was: "+Paraphrase.getPPDBScore("was", "verb"));
-        System.out.println("synonyms?: "+Paraphrase.checkSynonyms("arrive", "verb", "come", "verb"));
+        System.out.println("synonyms?: "+Paraphrase.checkSynonyms("arrive", "verb", "food", "noun"));
     }
 }
